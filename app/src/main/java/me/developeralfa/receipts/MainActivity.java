a3bfaf509dce26123c6c20bd02999b6ab43f2f7d
Nipun Aggarwal <nipundtu@gmail.com>
2018-05-31 11:10:42 +0530

    Task 1 Add Items Task 2 Save bill Task 3 Make the bill customizable Task 4 Include the Bar Code Scanner

---
+++ app/src/main/java/me/developeralfa/receipts/MainActivity.java
@@ -0,0 +1,20 @@
+package me.developeralfa.receipts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import me.developeralfa.receipts.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
